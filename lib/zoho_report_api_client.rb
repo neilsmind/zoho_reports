9dbcab5f932fc730e87b7f77c3ec27c8cae445c3
neil <neil@scorebrd.com>
2014-04-16 21:42:57 -0400

    fixed initial issues to get httparty working

--- lib/zoho_report_api_client.rb
+++ lib/zoho_report_api_client.rb
@@ -1,20 +1,24 @@
 require "zoho_report_api_client/version"
 require "uri"
+require "HTTParty"
 
 module ZohoReportApiClient
   class ReportClient
+    include HTTParty
     # ZohoReportAPIClient provides the python based language binding to the http based api of ZohoReports.
 
-    def init(login_email, authtoken)
-      # Creates a new C{ReportClient} instance.
-      # @param authtoken: User's authtoken.
-      # @type authtoken:string
-      self.iam_server_url = "https://accounts.zoho.com"
-      self.report_server_url = "https://reportsapi.zoho.com/api"
+    attr_accessor :authtoken, :login_email, :api_version
+
+    base_uri "reportsapi.zoho.com:443/api"
+
+    def initialize(login_email,authtoken)
       self.authtoken = authtoken
       self.login_email = login_email
       self.api_version = '1.0'
-      self.default_query = {
+    end
+
+    def default_query
+      {
         'authtoken' => self.authtoken,
         'ZOHO_OUTPUT_FORMAT' => 'JSON',
         'ZOHO_ERROR_FORMAT' => 'JSON',
@@ -22,9 +26,13 @@ module ZohoReportApiClient
       }
     end
 
-    def send_request(url, http_method, query, options={})
-      query = self.default_query.merge!(query)
-      response = self.class.send(http_method, "#{self.report_server_url}/#{login_email}/#{url}", options)
+    def send_request(url, http_method, options)
+      dq = default_query
+
+      # Merge our default query string values with the specificed query values
+      options[:query] = dq.merge!(options[:query])
+
+      response = self.class.send(http_method,url, options)
 
       if response.success?
         response
@@ -36,139 +44,160 @@ module ZohoReportApiClient
     # Gets copy database key for specified database identified by the URI    
     def get_copy_db_key(db_uri)
       # payLoad = ReportClientHelper.getAsPayLoad([config],None,None)
-      query = {
-        'ZOHO_ACTION' => 'GETCOPYDBKEY',
-      }
+      options = {
+        :query => { 'ZOHO_ACTION' => 'GETCOPYDBKEY' }
+      } 
 
-      send_request(db_uri, 'post', query)
+      send_request(db_uri, 'post', options)
     end
 
     # Copy the specified database identified by the URI
     def copy_database(db_uri)
-      query = {
-        'ZOHO_ACTION' => 'COPYDATABASE',
+      options = {
+        :query => { 'ZOHO_ACTION' => 'COPYDATABASE' }
       }
-      send_request(db_uri, 'post', query)
+
+      send_request(db_uri, 'post', options)
     end
 
     # Delete the specified database
     def delete_database(database_name)
-      query = {
-        'ZOHO_ACTION' => 'DELETEDATABASE',
-        'ZOHO_DATABASE_NAME' => database_name,
+      options = {
+        :query => {
+          'ZOHO_ACTION' => 'DELETEDATABASE',
+          'ZOHO_DATABASE_NAME' => database_name,
+        }
       }
-      send_request(user_uri, 'post', query)
+      send_request(user_uri, 'post', options)
     end
  
     # Add the users to the Zoho Reports Account
     def add_user(email_ids)
-      query = {
-        'ZOHO_ACTION' => 'ADDUSER',
-        'ZOHO_EMAILS' => email_ids,
+      options = {
+        :query => {
+          'ZOHO_ACTION' => 'ADDUSER',
+          'ZOHO_EMAILS' => email_ids,
+        }
       }
 
-      send_request(user_uri, 'post', query)
+      send_request(user_uri, 'post', options)
     end
  
     # Remove the users from the Zoho Reports Account
     def remove_user(email_ids)
-      query = {
-        'ZOHO_ACTION' => 'REMOVEUSER',
-        'ZOHO_EMAILS' => email_ids,
+      options = {
+        :query => {
+          'ZOHO_ACTION' => 'REMOVEUSER',
+          'ZOHO_EMAILS' => email_ids,
+        }
       }
 
-      send_request(user_uri, 'post', query)
+      send_request(user_uri, 'post', options)
     end
  
     # Activate the users in the Zoho Reports Account
     def activate_user(email_ids)
-      query = {
-        'ZOHO_ACTION' => 'ACTIVATEUSER',
-        'ZOHO_EMAILS' => email_ids,
+      options = {
+        :query => {
+          'ZOHO_ACTION' => 'ACTIVATEUSER',
+          'ZOHO_EMAILS' => email_ids,
+        }
       }
 
-      send_request(user_uri, 'post', query)
+      send_request(user_uri, 'post', options)
     end
  
     # Deactivate the users in the Zoho Reports Account
     def deactivate_user(email_ids)
-      query = {
-        'ZOHO_ACTION' => 'DEACTIVATEUSER',
-        'ZOHO_EMAILS' => email_ids,
+      options = {
+        :query => {
+          'ZOHO_ACTION' => 'DEACTIVATEUSER',
+          'ZOHO_EMAILS' => email_ids,
+        }
       }
 
-      send_request(user_uri, 'post', query)
+      send_request(user_uri, 'post', options)
     end
 
     # Adds a row to the specified table identified by the URI
     def add_row(table_uri, column_values)
-      query = {
-        'ZOHO_ACTION' => 'ADDROW',
+      options = {
+        :query => {
+          'ZOHO_ACTION' => 'ADDROW',
+        },
+        :body => column_values
       }
 
-      send_request(table_uri, 'post', query, body: body)
+      send_request(table_uri, 'post', options)
     end
     
     # Update the data in the specified table identified by the URI.
-    def update_row(table_uri, column_values, criteria, config={})
-      query = {
-        'ZOHO_ACTION' => 'UPDATE',
-      }
-
+    def update_data(table_uri, column_values, criteria, config={})
       body = column_values.merge!({'ZOHO_CRITERIA' => criteria})
       body = body.merge!(config) if config.present?
 
-      send_request(table_uri, 'post', query, body: body)
+      options = {
+        :query => {
+          'ZOHO_ACTION' => 'UPDATE',
+        },
+        :body => body
+      }
+
+      send_request(table_uri, 'post', options)
     end
 
     # Delete the data in the specified table identified by the URI.
-    def update_row(table_uri, column_values, criteria, config={})
-      query = {
-        'ZOHO_ACTION' => 'DELETE',
-      }
-
+    def delete_data(table_uri, criteria, config={})
       body = {'ZOHO_CRITERIA' => criteria}
       body = body.merge!(config) if config.present?
 
-      send_request(table_uri, 'post', query, body: body)
+      options = {
+        :query => {
+          'ZOHO_ACTION' => 'DELETE',
+        },
+        :body => body
+      }
+
+      send_request(table_uri, 'post', options)
     end
 
     # Export the data in the specified table identified by the URI.
     def export_data(table_or_report_uri, format, criteria, config={})
-      query = {
-        'ZOHO_ACTION' => 'EXPORT',
-        'ZOHO_OUTPUT_FORMAT' => format,
-      }
-
       body = {'ZOHO_CRITERIA' => criteria}
       body = body.merge!(config) if config.present?
 
-      send_request(table_or_report_uri, 'post', query, body: body)
+      options = {
+        :query => {
+          'ZOHO_ACTION' => 'EXPORT',
+          'ZOHO_OUTPUT_FORMAT' => format,
+        },
+        :body => body
+      }
+
+      send_request(table_or_report_uri, 'post', options)
       # TODO: Figure out to what to do with File objects response
     end
 
     # Export the data with the specified SQL query identified by the URI.
     def export_data_using_sql(table_or_report_uri, format, sql, config={})
-      query = {
-        'ZOHO_ACTION' => 'EXPORT',
-        'ZOHO_OUTPUT_FORMAT' => format,
-      }
-
       body = {'ZOHO_SQLQUERY' => sql}
       body = body.merge!(config) if config.present?
 
-      send_request(table_or_report_uri, 'post', query, body: body)
+      options = {
+        :query => {
+          'ZOHO_ACTION' => 'EXPORT',
+          'ZOHO_OUTPUT_FORMAT' => format,
+        },
+        :body => body
+      }
+
+      send_request(table_or_report_uri, 'post', options)
       # TODO: Figure out to what to do with File objectsw response
     end        
 
     # Bulk import data into the table identified by the URI.
     def import_data(table_uri, import_type, import_content, import_config={})
       raise "Import Type must be APPEND, TRUNCATEADD or UPDATEADD" unless  ["APPEND", "TRUNCATEADD", "UPDATEADD"].include?(import_type)
-
-      query = {
-        'ZOHO_ACTION' => 'EXPORT',
-        'ZOHO_OUTPUT_FORMAT' => format,
-      }
 
       body = {
         'ZOHO_AUTO_IDENTIFY' => 'true',
@@ -179,23 +208,32 @@ module ZohoReportApiClient
       }
       body = body.merge!(import_config) if import_config.present?
 
-      send_request(table_or_report_uri, 'post', query, body: body)
+      options = {
+        :query => {
+          'ZOHO_ACTION' => 'EXPORT',
+          'ZOHO_OUTPUT_FORMAT' => format,
+        },
+        :body => body
+      }
+
+      send_request(table_or_report_uri, 'post', options)
       # TODO: Figure out to what to do with File objectsw response
     end        
 
     # Returns the URI for the specified database table (or report).
-    def get_uri(db_owner_name, db_name, table_or_report_name)
-      "#{self.report_server_url}/api/#{URI.encode db_owner_name}/#{URI.encode db_name}/#{URI.encode table_or_report_name}"
+    def get_uri(db_name, table_or_report_name)
+      "/#{URI.encode self.login_email}/#{URI.encode db_name}/#{URI.encode table_or_report_name}"
     end        
 
     # Returns the URI for the specified database 
-    def get_db_uri(db_owner_name, db_name)
-      "#{self.report_server_url}/api/#{URI.encode db_owner_name}/#{URI.encode db_name}"
+    def get_db_uri(db_name)
+      "#{URI.encode self.login_email}/#{URI.encode db_name}"
     end
 
     # Returns the URI for the specified user 
-    def get_user_uri(db_owner_name)
-      "#{self.report_server_url}/api/#{URI.encode db_owner_name}"
+    def get_user_uri()
+      "/#{URI.encode self.login_email}"
     end
+  end
 
 end
